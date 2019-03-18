cfed2f82cbc1af69d17ceefbaeeb03994ee41ef0
CrmbL <axelsch@hotmail.fr>
2019-03-18 23:43:12 +0100

    Initial commit

---
+++ TheVault/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+
+namespace TheVault
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartServer(object sender, RoutedEventArgs e)
+        {
+            AsynchronousSocketListener.StartListening();
+        }
+
+        private void SendMessage(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
